5eae608533cc7e6514d4e8391f167d80f7b2a932
a-chetvert <a.chetvert@gmail.com>
2026-01-28 01:45:58 +0300

    выполнены задания cppm-homeworks/2.1/ получены по ссылке: https://github.com/netology-code/cppm-homeworks/tree/main/02/01

---
+++ cppm-homeworks/02/1/lesson_02_01/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,82 @@
+﻿/**
+ * @brief Задача 1. Месяцы
+ * @note  https://github.com/netology-code/cppm-homeworks/tree/main/02/01
+ */
+#include <iostream>
+#include <locale>
+#include <windows.h>
+
+enum class month {
+    january = 1,
+    february,
+    march,
+    april,
+    may,
+    june,
+    july,
+    august,
+    september,
+    october,
+    november,
+    december
+};
+
+int main() {
+    setlocale(LC_ALL, "en_US.UTF-8");
+
+    int numMonth{0};
+    month nowMonth{0};
+
+    do{
+
+        std::cout << "Введите номер месяца: ";
+        std::cin >> numMonth;
+        
+        if (numMonth != 0) {
+            nowMonth = static_cast<month>(numMonth);
+
+            switch (static_cast<int>(nowMonth)) {
+                case 1: 
+                    std::cout << "Январь" << std::endl;
+                break;
+                case 2:
+                    std::cout << "Февраль" << std::endl;
+                break;
+                case 3:
+                    std::cout << "Март" << std::endl;
+                break;
+                case 4:
+                    std::cout << "Апрель" << std::endl;
+                break;
+                case 5:
+                    std::cout << "Май" << std::endl;
+                break;
+                case 6:
+                    std::cout << "Июнь" << std::endl;
+                break;
+                case 7:
+                    std::cout << "Июль" << std::endl;
+                break;
+                case 8:
+                    std::cout << "Август" << std::endl;
+                break;
+                case 9:
+                    std::cout << "Сентябрь" << std::endl;
+                break;
+                case 10:
+                    std::cout << "Октябрь" << std::endl;
+                break;
+                case 11:
+                    std::cout << "Ноябрь" << std::endl;
+                break;
+                case 12:
+                    std::cout << "Декабрь" << std::endl;
+                break;
+                default:
+                    std::cout << "Неправильный номер!" << std::endl;
+            }
+        }
+    } while (numMonth);
+    std::cout << "До свидания";
+    return 0;
+}
