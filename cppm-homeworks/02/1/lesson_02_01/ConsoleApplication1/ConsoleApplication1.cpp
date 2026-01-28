bd6b5e36fd8dc92b36792d216c19053cebe50b46
a-chetvert <a.chetvert@gmail.com>
2026-01-28 19:28:45 +0300

    исправлено задание cppm-homeworks/2.1/ получено по ссылке: https://github.com/netology-code/cppm-homeworks/tree/main/02/01

--- cppm-homeworks/02/1/lesson_02_01/ConsoleApplication1/ConsoleApplication1.cpp
+++ cppm-homeworks/02/1/lesson_02_01/ConsoleApplication1/ConsoleApplication1.cpp
@@ -24,59 +24,69 @@ enum class month {
 int main() {
     setlocale(LC_ALL, "en_US.UTF-8");
 
-    int numMonth{0};
-    month nowMonth{0};
+    int input{0};
+    const int endInput = 0;
 
-    do{
-
+    while (true) {
         std::cout << "Введите номер месяца: ";
-        std::cin >> numMonth;
-        
-        if (numMonth != 0) {
-            nowMonth = static_cast<month>(numMonth);
+        // получаем от пользователя номер месяца
+        std::cin >> input;
 
-            switch (static_cast<int>(nowMonth)) {
-                case 1: 
-                    std::cout << "Январь" << std::endl;
-                break;
-                case 2:
-                    std::cout << "Февраль" << std::endl;
-                break;
-                case 3:
-                    std::cout << "Март" << std::endl;
-                break;
-                case 4:
-                    std::cout << "Апрель" << std::endl;
-                break;
-                case 5:
-                    std::cout << "Май" << std::endl;
-                break;
-                case 6:
-                    std::cout << "Июнь" << std::endl;
-                break;
-                case 7:
-                    std::cout << "Июль" << std::endl;
-                break;
-                case 8:
-                    std::cout << "Август" << std::endl;
-                break;
-                case 9:
-                    std::cout << "Сентябрь" << std::endl;
-                break;
-                case 10:
-                    std::cout << "Октябрь" << std::endl;
-                break;
-                case 11:
-                    std::cout << "Ноябрь" << std::endl;
-                break;
-                case 12:
-                    std::cout << "Декабрь" << std::endl;
-                break;
-                default:
-                    std::cout << "Неправильный номер!" << std::endl;
-            }
+        if ((input > static_cast<int>(month::december))|| 
+            (input < endInput))
+        {
+            std::cout << "Неправильный номер!" << std::endl;
+            continue;
         }
-    } while (numMonth);
-    std::cout << "До свидания";
+        else if (input == endInput)
+        {
+            std::cout << "До свидания";
+            break;
+        }
+
+        //  переводим в месяцы 
+        month inputMonth = static_cast<month>(input);
+
+        switch (static_cast<int>(inputMonth)) {
+            case 1:
+                std::cout << "Январь" << std::endl;
+                break;
+            case 2:
+                std::cout << "Февраль" << std::endl;
+                break;
+            case 3:
+                std::cout << "Март" << std::endl;
+                break;
+            case 4:
+                std::cout << "Апрель" << std::endl;
+                break;
+            case 5:
+                std::cout << "Май" << std::endl;
+                break;
+            case 6:
+                std::cout << "Июнь" << std::endl;
+                break;
+            case 7:
+                std::cout << "Июль" << std::endl;
+                break;
+            case 8:
+                std::cout << "Август" << std::endl;
+                break;
+            case 9:
+                std::cout << "Сентябрь" << std::endl;
+                break;
+            case 10:
+                std::cout << "Октябрь" << std::endl;
+                break;
+            case 11:
+                std::cout << "Ноябрь" << std::endl;
+                break;
+            case 12:
+                std::cout << "Декабрь" << std::endl;
+                break;
+            default:
+                std::cout << "Неправильный номер!" << std::endl;
+        }
+    }
     return 0;
 }
