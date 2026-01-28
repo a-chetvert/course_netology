ddaff3c7be80c0d65960885a75245f1982164087
a-chetvert <a.chetvert@gmail.com>
2026-01-28 19:56:47 +0300

    заготовка под вторую задачу

---
+++ cppm-homeworks/02/2/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,30 @@
+﻿/**
+ * @brief Задача 1. Месяцы
+ * @note  https://github.com/netology-code/cppm-homeworks/tree/main/02/01
+ */
+#include <iostream>
+#include <locale>
+#include <windows.h>
+/**
+* @brief структура для хранения информации о банковском счёте
+*/
+struct account {
+    int accountNumber;
+    std::string ownerSName;
+    float amount;
+};
+
+void printAccountData(struct account &acc_) {
+    std::cout << "Ваш счёт: " << acc_.ownerSName << ", " << acc_.accountNumber << ", " << acc_.amount;
+}
+
+int main() {
+    setlocale(LC_ALL, "en_US.UTF-8");
+
+    int input{0};
+    const int endInput = 0;
+    struct account newAccount {123, "asd", 3.4 };
+
+
+    return 0;
+}
