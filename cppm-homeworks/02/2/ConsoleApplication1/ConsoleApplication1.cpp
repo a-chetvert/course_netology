f366f1ed281d40fb946e9e094a1a3304171a1919
a-chetvert <a.chetvert@gmail.com>
2026-01-30 02:18:24 +0300

    исправлено задание cppm-homeworks/2.1/ получено по ссылке: https://github.com/netology-code/cppm-homeworks/tree/main/02/01
    выполнены задания 2.2 и 2.3
    https://github.com/netology-code/cppm-homeworks/tree/main/02/02
    https://github.com/netology-code/cppm-homeworks/tree/main/02/03

--- cppm-homeworks/02/2/ConsoleApplication1/ConsoleApplication1.cpp
+++ cppm-homeworks/02/2/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,30 +1,70 @@
 ﻿/**
- * @brief Задача 1. Месяцы
- * @note  https://github.com/netology-code/cppm-homeworks/tree/main/02/01
+ * @brief Задача 2. Счета
+ * @note  https://github.com/netology-code/cppm-homeworks/tree/main/02/02
  */
 #include <iostream>
-#include <locale>
+#include <string> // Для std::wstring и std::getline
 #include <windows.h>
+#include <io.h>
+#include <fcntl.h>
+
 /**
 * @brief структура для хранения информации о банковском счёте
 */
 struct account {
     int accountNumber;
-    std::string ownerSName;
+    std::wstring ownerSName;
     float amount;
 };
 
-void printAccountData(struct account &acc_) {
-    std::cout << "Ваш счёт: " << acc_.ownerSName << ", " << acc_.accountNumber << ", " << acc_.amount;
-}
+void changeAmount(account& account_, float newAmount);
+void printAccountData(struct account& acc_);
 
 int main() {
-    setlocale(LC_ALL, "en_US.UTF-8");
+    struct account newAccount { 0, L"TEST", 1.23 };
+    float newAmount{ 0 };
 
-    int input{0};
-    const int endInput = 0;
-    struct account newAccount {123, "asd", 3.4 };
+    // Установка режима для Unicode
+    _setmode(_fileno(stdout), _O_U16TEXT);
+    _setmode(_fileno(stdin), _O_U16TEXT);
 
+    std::wcout << L"Введите номер счёта: ";
+    std::wcin >> newAccount.accountNumber;
+    std::wcin.ignore();
 
-    return 0;
+    std::wcout << L"Введите имя владельца: ";
+    std::getline(std::wcin, newAccount.ownerSName);  // Используем wcin для wstring
+
+    
+    std::wcout << L"Введите баланс: ";
+    std::wcin >> newAccount.amount;
+
+    std::wcout << L"Введите новый баланс: ";
+    std::wcin >> newAmount;
+
+    changeAmount(newAccount, newAmount);
+
+    printAccountData(newAccount);
+
+    return EXIT_SUCCESS;
+}
+
+/**
+ * @brief Изменяет баланс банковского счета
+ *
+ * @param[in,out] account_ Ссылка на структуру банковского счета, который нужно изменить
+ * @param[in] newAmount Новое значение баланса 
+ */
+void changeAmount(account& account_, float newAmount) {
+    account_.amount = newAmount;
+}
+
+/**
+ * @brief Выводит информацию о банковском счете в консоль
+ * 
+ * @param[in] acc_ Константная ссылка на структуру банковского счета для вывода *
+ */
+void printAccountData(struct account& acc_) {
+    std::wcout << L"Ваш счёт: " << acc_.ownerSName;
+    std::wcout << ", " << acc_.accountNumber << ", " << acc_.amount;
 }
---
+++ cppm-homeworks/02/3/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,81 @@
+﻿/**
+ * @brief Задача 3. Вывод структуры
+ * @note  https://github.com/netology-code/cppm-homeworks/tree/main/02/03
+ */
+#include <string>
+#include <windows.h>
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <vector>
+
+struct adress {
+    std::string city;
+    std::string street;
+    int house;
+    int flat;
+    int index;
+};
+
+void printAdress(struct adress& printAdress);
+std::string utf8To1251(const std::string& utf8Str);
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    struct adress firstAdr { utf8To1251("Москва"), utf8To1251("Арбат"), 12, 8, 123456 },
+                  secondAdr{ utf8To1251("Ижевск"), utf8To1251("Пушкина"), 59, 143, 953769 };
+    
+    printAdress(firstAdr);
+    printAdress(secondAdr);
+
+    return EXIT_SUCCESS;
+}
+
+/**
+ * @brief Выводит информацию об адресе
+ *
+ * @param[in] printAdress Ссылка на структуру адреса для вывода
+ */
+void printAdress(struct adress& printAdress) {
+    std::string label1 = utf8To1251("Город: ");
+    std::string label2 = utf8To1251("Улица: ");
+    std::string label3 = utf8To1251("Номер дома: ");
+    std::string label4 = utf8To1251("Номер квартиры: ");
+    std::string label5 = utf8To1251("Индекс: ");
+    
+    std::cout << label1 << printAdress.city << std::endl;
+    std::cout << label2 << printAdress.street << std::endl;
+    std::cout << label3 << printAdress.house << std::endl;
+    std::cout << label4 << printAdress.flat << std::endl;
+    std::cout << label5 << printAdress.index << std::endl;
+    std::cout << "\n";
+}
+
+/**
+ * @brief Конвертирует строку из кодировки UTF-8 в Windows-1251
+ *
+ * @param[in] utf8Str Строка в кодировке UTF-8 для конвертации
+ * @return std::string Строка в кодировке Windows-1251
+ * @retval Пустая строка В случае ошибки преобразования
+ *
+ * @details Функция выполняет двухэтапное преобразование кодировки:
+ * 1. UTF-8 → UTF-16 (широкие символы)
+ * 2. UTF-16 → Windows-1251
+ */
+std::string utf8To1251(const std::string& utf8Str) {
+    int wsize = MultiByteToWideChar(CP_UTF8, 0, utf8Str.c_str(), -1, NULL, 0);
+    if (wsize == 0) return "";
+
+    std::vector<wchar_t> wstr(wsize);
+    MultiByteToWideChar(CP_UTF8, 0, utf8Str.c_str(), -1, wstr.data(), wsize);
+
+    int size = WideCharToMultiByte(1251, 0, wstr.data(), -1, NULL, 0, NULL, NULL);
+    if (size == 0) return "";
+
+    std::vector<char> str1251(size);
+    WideCharToMultiByte(1251, 0, wstr.data(), -1, str1251.data(), size, NULL, NULL);
+
+    return std::string(str1251.data(), size - 1);
+}
