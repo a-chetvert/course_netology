45977c413093aabb195f16f05bd5e4e6f1b1ef8b
a-chetvert <a.chetvert@gmail.com>
2026-02-04 18:58:28 +0300

    исправлено задание cppm-homeworks: 5.1

--- cppm-homeworks/05/1/lesson_5_1.cpp
+++ cppm-homeworks/05/1/lesson_5_1.cpp
@@ -4,24 +4,27 @@
  */
 #include <windows.h>
 #include <iostream>
+#include <string>
 
 class Figure {
 protected:
-    int sidesCount;
+    int sidesCount;     ///< число сторон
+    std::string name;   ///< название фигуры
 
 public:
+    std::string get_name();
     int get_sides_count();
-    Figure(int sidesCount = 0);
+    Figure(int sidesCount = 0, std::string name = "Фигура");
 };
 
 class Triangle : public Figure {
 public:
-    Triangle() : Figure(3) {};
+    Triangle() : Figure(3, "Треугольник") {};
 };
 
 class Quadrangle : public Figure {
 public:
-    Quadrangle() : Figure(4) {};
+    Quadrangle() : Figure(4, "Четырёхугольник") {};
 };
 
 int main() {
@@ -33,18 +36,23 @@ int main() {
     Quadrangle newQuadrangle;
 
     std::cout << "Количество сторон:" << std::endl;
-    std::cout << "Фигура: ";
+    std::cout << newFigure.get_name() << ": ";
     std::cout << newFigure.get_sides_count() << std::endl;
-    std::cout << "Треугольник: ";
+    std::cout << newTriangle.get_name() << ": ";
     std::cout << newTriangle.get_sides_count() << std::endl;
-    std::cout << "Четырёхугольник: ";
+    std::cout << newQuadrangle.get_name() << ": ";
     std::cout << newQuadrangle.get_sides_count() << std::endl;
 
     return EXIT_SUCCESS;
 }
 
-Figure::Figure(int sidesCount) {
+Figure::Figure(int sidesCount, std::string name) {
     this->sidesCount = sidesCount;
+    this->name = name;
+}
+
+std::string Figure::get_name(){
+    return name;
 }
 
 int Figure::get_sides_count() {
