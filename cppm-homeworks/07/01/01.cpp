7698feb1a085cf3d96beb83ec193099a1fd27f59
a-chetvert <a.chetvert@gmail.com>
2026-02-08 00:32:52 +0300

    выполнены задания cppm-homeworks: 7.1 и 7.2 получены по ссылке https://github.com/netology-code/cppm-homeworks/tree/main/07/01 https://github.com/netology-code/cppm-homeworks/tree/main/07/02

---
+++ cppm-homeworks/07/01/01.cpp
@@ -0,0 +1,43 @@
+﻿/**
+ * @brief Задача 1. Препроцессорные директивы
+ * @note  https://github.com/netology-code/cppm-homeworks/tree/main/07/01
+ */
+#include <iostream>
+
+/// @brief варианты режимов
+#define MODE_TEST 0
+#define MODE_COMBAT 1
+#define MODE_OTHER 2
+
+/// @brief выбор режима
+#define MODE MODE_TEST
+
+// если MODE не определен
+#ifndef MODE 
+#error Macro MODE required 
+#endif 
+
+// определение функции для боевого режима
+#if MODE == MODE_COMBAT
+int add(int x, int y) {
+    return x + y;
+}
+#endif 
+
+int main() {
+#if MODE == MODE_TEST
+    std::cout << "Работаю в режиме тренировки" << std::endl;
+#elif MODE == MODE_COMBAT
+    int x{ 0 }, y{ 0 };
+    std::cout << "Работаю в боевом режиме" << std::endl;
+    std::cout << "Введите число 1: ";
+    std::cin >> x;
+    std::cout << "Введите число 2: ";
+    std::cin >> y;
+    std::cout << "Результат сложения: " << add(x, y) << std::endl;
+#else
+    std::cout << "Неизвестный режим. Завершение работы" << std::endl;
+#endif 
+
+    return EXIT_SUCCESS;
+}
---
+++ cppm-homeworks/07/02/02.cpp
@@ -0,0 +1,18 @@
+﻿/**
+ * @brief Задача 2. Макрос SUB
+ * @note  https://github.com/netology-code/cppm-homeworks/tree/main/07/02
+ */
+#include <iostream>
+
+#define SUB(a, b) ((a)-(b))
+ 
+int main() {
+	int a = 6;
+	int b = 5;
+	int c = 2;
+	std::cout << SUB(a, b) << std::endl;
+	std::cout << SUB(a, b) * c << std::endl;
+	std::cout << SUB(a, b + c) * c << std::endl;
+
+    return EXIT_SUCCESS;
+}
