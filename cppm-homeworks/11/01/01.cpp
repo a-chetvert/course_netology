3614fe381c0f1a45c4eca260768fbc9ca1f17b8f
a-chetvert <a.chetvert@gmail.com>
2026-02-17 02:00:56 +0300

    выполнено задание cppm-homeworks/11.1/ получено по ссылке: https://github.com/netology-code/cppm-homeworks/tree/main/11/01

---
+++ cppm-homeworks/11/01/01.cpp
@@ -0,0 +1,22 @@
+﻿/**
+ * @brief Задача 1. Статическая библиотека
+ * @note  https://github.com/netology-code/cppm-homeworks/tree/main/11/01
+ */
+#include <iostream>
+#include "StaticLib\StaticLib.h"
+#include <windows.h> 
+#include <sstream>
+#include <string>
+#include <vector>
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    Greeter greeter;
+    std::string tmp;
+    std::cout << utf8To1251("Введите имя: ");
+    std::cin >> tmp;
+    greeter.greet(tmp);
+}
---
+++ cppm-homeworks/11/01/StaticLib/StaticLib.h
@@ -0,0 +1,19 @@
+﻿﻿/**
+ * @brief Задача 1. Статическая библиотека
+ * @note  https://github.com/netology-code/cppm-homeworks/tree/main/11/01
+ */
+
+#pragma once
+#include <iostream>
+
+class Greeter
+{
+	public:
+	Greeter();
+	void greet(std::string name);
+};
+
+std::string cp1251ToUtf8(const std::string& cp1251Str);
+
+std::string utf8To1251(const std::string& utf8Str);
+
