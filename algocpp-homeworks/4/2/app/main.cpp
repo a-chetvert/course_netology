e5ad8847aa59f15bcfa698db68dd75cdca152bbe
a-chetvert <a.chetvert@gmail.com>
2026-04-23 23:50:56 +0300

    выполнено задание algocpp-homeworks 4.2

--- algocpp-homeworks/4/2/app/main.cpp
+++ algocpp-homeworks/4/2/app/main.cpp
@@ -14,7 +14,7 @@ struct DynArr {
 };
 
 void print_dynamic_array(int* arr, int logical_size, int actual_size) {
-  std::cout << "Динамический массив:";
+  std::cout << "массив:";
   for (int i = 0; i < logical_size; i++) {
     std::cout << " " << arr[i];
   }
@@ -23,17 +23,31 @@ void print_dynamic_array(int* arr, int logical_size, int actual_size) {
   }
 }
 
-void append_to_dynamic_array(int var_to_app, int* arr, int logical_size, int actual_size) {
-  if (actual_size > logical_size) {
-    arr[logical_size++] = var_to_app;
+/**
+ * @brief Добавляет элемент в конец динамического массив с расширением
+ * @param var_to_app элемент для добавления
+ * @param arr динамический массив
+ * @param logical_size логический размер
+ * @param actual_size фактический размер
+ */
+void append_to_dynamic_array(const int var_to_app, int*& arr, int& logical_size, int& actual_size) {
+  if (logical_size < actual_size) {
+    arr[logical_size] = var_to_app;
+    logical_size++;
   }
   else if (actual_size == logical_size) {
-    new int[actual_size * 2];
-
+    int new_actual_size = actual_size * 2;
+    int* arrTmp = new int[new_actual_size];
+    for (int i = 0; i < actual_size; i++) {
+      arrTmp[i] = arr[i];
+    }
+    arrTmp[logical_size] = var_to_app;
+    delete[] arr;
+    arr = arrTmp;
+    logical_size++;
+    actual_size = new_actual_size;
   }
 }
-
-
 
 int main() {
   system("chcp 65001");
@@ -58,6 +72,22 @@ int main() {
     std::cin >> dynArr.arr[i];
   }
 
+  std::cout << "Динамический ";
+  print_dynamic_array(dynArr.arr, dynArr.logical_size, dynArr.actual_size);
+
+  int element_for_app{ 1 };
+  while (element_for_app) {
+    std::cout << "\nВведите элемент для добавления: ";
+    std::cin >> element_for_app;
+    if (element_for_app == 0) {
+      break;
+    }
+    append_to_dynamic_array(element_for_app, dynArr.arr, dynArr.logical_size, dynArr.actual_size);
+    std::cout << "Динамический ";
+    print_dynamic_array(dynArr.arr, dynArr.logical_size, dynArr.actual_size);
+  }
+
+  std::cout << "Спасибо! Ваш ";
   print_dynamic_array(dynArr.arr, dynArr.logical_size, dynArr.actual_size);
 
   delete[] dynArr.arr;
